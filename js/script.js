161bf8f7d4b4ba7642cebf0b975a18690b82d02c
Mattia Capelli <mattia@mattiacapelli.it>
2022-10-10 18:19:04 +0200

    .

--- js/script.js
+++ js/script.js
@@ -21,9 +21,9 @@ function getNumPeople(value)
 }
 
 function calculate() {
-    if(percentage == null || total == null || numpeople == null || percentage == "" || total == "" || numpeople == "") 
+    if(percentage == null || total == null || numpeople == null || percentage == "" || total == "" || numpeople == "" || percentage <= 0 || total <= 0 || numpeople <= 0) 
     {
-        alert("Please enter all values");
+        alert("Please enter correct values");
     }
     else
     {
@@ -47,4 +47,74 @@ function returnback() {
     percentage = null;
     total = null;
     numpeople = null;
+}
+
+function activate(id) {
+    var element1 = document.getElementById('no');
+    var element2 = document.getElementById('5');
+    var element3 = document.getElementById('10');
+    var element4 = document.getElementById('15');
+    var element5 = document.getElementById('20');
+    var element6 = document.getElementById('25');
+
+    switch (id) {
+        case 'no':
+            element1.classList.add("active");
+            element2.classList.remove("active");
+            element3.classList.remove("active");
+            element4.classList.remove("active");
+            element5.classList.remove("active");
+            element6.classList.remove("active");
+            break;
+        
+        case '5':
+            element1.classList.remove("active");
+            element2.classList.add("active");
+            element3.classList.remove("active");
+            element4.classList.remove("active");
+            element5.classList.remove("active");
+            element6.classList.remove("active");
+            break;
+
+        case '10':
+            element1.classList.remove("active");
+            element2.classList.remove("active");
+            element3.classList.add("active");
+            element4.classList.remove("active");
+            element5.classList.remove("active");
+            element6.classList.remove("active");
+            break;
+        
+        case '15':
+            element1.classList.remove("active");
+            element2.classList.remove("active");
+            element3.classList.remove("active");
+            element4.classList.add("active");
+            element5.classList.remove("active");
+            element6.classList.remove("active");
+            break;
+
+        case '20':
+            element1.classList.remove("active");
+            element2.classList.remove("active");
+            element3.classList.remove("active");
+            element4.classList.remove("active");
+            element5.classList.add("active");
+            element6.classList.remove("active");
+            break;
+
+        case '25':
+            element1.classList.remove("active");
+            element2.classList.remove("active");
+            element3.classList.remove("active");
+            element4.classList.remove("active");
+            element5.classList.remove("active");
+            element6.classList.add("active");
+            break;
+
+        default:
+            console.log("Error");
+            break;
+
+    }
 }
